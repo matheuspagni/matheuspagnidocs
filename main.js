740d45e67c5708d13d53f600817947bd39b7efb8
Matheus Gambare Pagni <re034486@resource.com.br>
2018-04-24 16:40:37 -0300

    commit2

--- main.js
+++ main.js
@@ -17,15 +17,31 @@ var email   = document.getElementById('Email');
 var senha    = document.getElementById('Passwd');
 
 reviewForm.addEventListener('submit', (e) => {
+  console.log('1');
   e.preventDefault();
 
   var id = '1'
 
-  db.ref('users/' + id).set({
+  db.ref().set({
     email: email.value,
     senha: senha.value
   });
 
   email.value = '';
   senha.value  = '';
+  console.log('2');
 });
+// var user_name = document.getElementById('user_name').value;
+  
+//    var uid = firebase.database().ref().child('users').push().key;
+   
+//    var data = {
+//     user_id: uid,
+//     user_name: user_name
+//    }
+   
+//    var updates = {};
+//    updates['/users/' + uid] = data;
+//    firebase.database().ref().update(updates);
+   
+//    alert('The user is created successfully!');
